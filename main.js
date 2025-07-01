85947a525493a11b8ae4140b9c78d2790254d367
Sebastian Senmache <87740017+luminate123@users.noreply.github.com>
2025-06-30 23:51:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 onload = () =>{
     document.body.classList.remove("container");
 };
+
+setTimeout(() => {
+  const mensaje = document.getElementById('mensaje-amor');
+  mensaje.style.display = 'flex';
+}, 5000);
+
+function cerrarMensaje() {
+  document.getElementById('mensaje-amor').style.display = 'none';
+}
